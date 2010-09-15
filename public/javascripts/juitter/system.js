701df526b8ba58f454b31bf34218a2976da5526a
Leonardo Tartari <ltartari@gmail.com>
2010-09-15 19:16:48 -0300

    loader.gif ao invés de Carregando mensagens...

--- public/javascripts/juitter/system.js
+++ public/javascripts/juitter/system.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
     lang:"en", // restricts the search by the given language
     live:"live-10", // the number after "live-" indicates the time in seconds to wait before request the Twitter API for updates.
     placeHolder:"softa_twitter_rss", // Set a place holder DIV which will receive the list of tweets example <div id="juitterContainer"></div>
-    loadMSG: "Carregando mensagens...", // Loading message, if you want to show an image, fill it with "image/gif" and go to the next variable to set which image you want to use on 
-    imgName: "loader.png", // Loading image, to enable it, go to the loadMSG var above and change it to "image/gif"
+    loadMSG: "image/gif", // Loading message, if you want to show an image, fill it with "image/gif" and go to the next variable to set which image you want to use on 
+    imgName: "loader.gif", // Loading image, to enable it, go to the loadMSG var above and change it to "image/gif"
     total: 7, // number of tweets to be show - max 100
     readMore: "link permanente »", // read more message to be show after the tweet content
     nameUser:"image", // insert "image" to show avatar of "text" to show the name of the user that sent the tweet 
