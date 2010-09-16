bb6a4736db3df9cd6f08ec1fb1c86f54187ef51d
Juan Maiz <juanmaiz@gmail.com>
2010-09-16 19:56:11 -0300

    ...

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,34 +5,8 @@ class SiteController < ApplicationController
   end
 
   def index
-<<<<<<< HEAD
     I18n.locale = params[:lang]
     @events = []
-=======
-=begin
-    require 'open-uri'
-    require 'ostruct'
-    feed = 'http://www.google.com/calendar/feeds/62loik9tem085k7v8rrgjg0138%40group.calendar.google.com/public/basic'
-    doc = Hpricot.XML(open(feed).read)
-    @events = []
-    doc.search('entry').each do |entry|
-        content = (entry%'content').html
-            whn = content.scan(/Quando: (.+201\d)/).to_s
-            whr = content.scan(/Onde: (.+)/).to_s
-           desc = content.scan(/Descrição do evento: (.+)/).to_s
-           link = content.scan(/http:\/\//).to_s
-      event_url = content.scan(/http:\/\//).to_s
-      @events << OpenStruct.new({
-        :title     => (entry%'title').html,
-        :when      => whn,
-        :where     => whr,
-        :desc      => desc,
-        :link      => link,
-        :event_url => event_url
-      })
-    end
-=end
->>>>>>> c899cd0a9d77b5933bcdc500e733b6b9c6291add
   end
 
 protected
