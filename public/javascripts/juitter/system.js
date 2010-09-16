350dcddfcbb76964fd08ec5ba6120258653a2437
Leonardo Tartari <ltartari@gmail.com>
2010-09-16 12:00:50 -0300

    lotsa stuff

--- public/javascripts/juitter/system.js
+++ public/javascripts/juitter/system.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
     live:"live-10", // the number after "live-" indicates the time in seconds to wait before request the Twitter API for updates.
     placeHolder:"softa_twitter_rss", // Set a place holder DIV which will receive the list of tweets example <div id="juitterContainer"></div>
     loadMSG: "image/gif", // Loading message, if you want to show an image, fill it with "image/gif" and go to the next variable to set which image you want to use on 
-    imgName: "loader.gif", // Loading image, to enable it, go to the loadMSG var above and change it to "image/gif"
-    total: 7, // number of tweets to be show - max 100
+    imgName: "/images/loader.gif", // Loading image, to enable it, go to the loadMSG var above and change it to "image/gif"
+    total: 5, // number of tweets to be show - max 100
     readMore: "link permanente »", // read more message to be show after the tweet content
     nameUser:"image", // insert "image" to show avatar of "text" to show the name of the user that sent the tweet 
     openExternalLinks:"newWindow", // here you can choose how to open link to external websites, "newWindow" or "sameWindow"
