2f19acd0197de4af74e4b925c059585984b27d7c
Leonardo Tartari <ltartari@gmail.com>
2010-09-19 19:35:56 -0300

    whatthecommit.com » Moved something to somewhere... goodnight...

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -4,8 +4,12 @@ module SiteHelper
     link_to "Twitter", "http://twitter.com/#{username}", :target => :_blank
   end
 
+  def tweet_this(message)
+    link_to t('labs.tweet_this'), "http://twitter.com/?status=#{CGI.escape(message)}", :target => :_blank
+  end
+
   def gravatar(email)
-   require 'digest/md5'
+    require 'digest/md5'
     gravatar_id = Digest::MD5.hexdigest(email+'@softa.com.br').to_s
     image_tag 'http://www.gravatar.com/avatar/' + gravatar_id + '?s=84'
   end
