f824c9cedf35d1f29c4c51aa12ada246191a41b8
Leonardo Tartari <ltartari@gmail.com>
2010-09-27 11:16:18 -0300

    <hr> to <br>

--- app/mailers/site.rb
+++ app/mailers/site.rb
@@ -7,6 +7,6 @@ class Site < ActionMailer::Base
   #   en.site.contact.subject
   #
   def contact(params)
-    mail :from => params[:email], :to => "contato@softa.com.br", :body => "<div style='font-family:helvetica,arial,sans-serif; font-size:14px;'><h2>contato no site softa.com.br</h2><h3 style='color:#444;'>email:</h3><h2>#{params[:email]}</h2><hr><h3 style='color:#444;'>nome:</h3><h2>#{params[:name]}</h2><hr><h3 style='color:#444;'>mensagem:</h3><p style='font-size:16px; color:#444'>#{params[:message]}</p><hr><h3 style='color:#444;'>código de autenticação (not for robots):</h3><code>#{params[:authenticity_token]}</code></div>"
+    mail :from => params[:email], :to => "contato@softa.com.br", :body => "<div style='font-family:helvetica,arial,sans-serif; font-size:14px;'><h2>contato no site softa.com.br</h2><h3 style='color:#444;'>email:</h3><h2>#{params[:email]}</h2><br><h3 style='color:#444;'>nome:</h3><h2>#{params[:name]}</h2><br><h3 style='color:#444;'>mensagem:</h3><p style='font-size:16px; color:#444'>#{params[:message]}</p><br><h3 style='color:#444;'>código de autenticação (not for robots):</h3><code>#{params[:authenticity_token]}</code></div>"
   end
 end
