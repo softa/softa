34301287e6d02183b23832be855790214e5f009e
Leonardo Tartari <ltartari@gmail.com>
2010-10-30 10:42:50 -0200

    cleaning up.

--- public/javascripts/juitter/system.js
+++ public/javascripts/juitter/system.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.Juitter.start({
     searchType:"fromUser", // needed, you can use "searchWord", "fromUser", "toUser"
-    searchObject:"pedroaxl,leotartari,joaomilho,danielweinmann,felipebcabral,dbiazus", // needed, you can insert a username here or a word to be searched for, if you wish multiple search, separate the words by comma.
+    searchObject:"pedroaxl,joaomilho,danielweinmann,felipebcabral,dbiazus", // needed, you can insert a username here or a word to be searched for, if you wish multiple search, separate the words by comma.
     // The values below will overwrite the ones on the Juitter default configuration. 
     // They are optional here.
     // I'm changing here as a example only
